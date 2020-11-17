78f18ad5ab21f8eb5ee9244fc394ce43ebaba9a5
aremartins <arealbmartinsan@gmail.com>
2020-11-17 09:28:41 -0300

    Inclusão exercicio Manipulacao Dom

--- sistemademultas/script.js
+++ sistemademultas/script.js
@@ -2,8 +2,9 @@ function calcular() {
     var vel = document.querySelector('input#txtn')
     var v = Number(vel.value)
     var res = document.querySelector('div#res')
-
+    res.innerHTML = `A velocidade verificada foi de <strong>${v} KM/h</strong>.`
     if (v >  110) {
-        res.innerHTML = `A velocidade verificada foi de ${v} km/h. Veículo multado!`
+        res.innerHTML += ' Veículo multado!'
     }
+    res.innerHTML += '<p>Dirija sempre com cinto de segurança!</p>'
 }
